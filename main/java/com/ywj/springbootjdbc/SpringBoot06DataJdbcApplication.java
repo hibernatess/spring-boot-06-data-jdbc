67b0386c9f1ba596db3435c538e0f93068fcd974
hibernatess <1959227206@qq.com>
2019-02-23 09:08:38 +0800

    okmok

---
+++ main/java/com/ywj/springbootjdbc/SpringBoot06DataJdbcApplication.java
@@ -0,0 +1,17 @@
+package com.ywj.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class SpringBoot06DataJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot06DataJdbcApplication.class, args);
+    }
+
+}
